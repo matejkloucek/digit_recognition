d078f6e6645dcfb0f80e31fa3f6ce48b4c416f56
Matej Kloucek <kloucmat@fjfi.cvut.cz>
2022-04-11 14:56:08 +0200

    center frame

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tensorflow.keras import layers
 import cv2 as cv
 
 
-threshold_val = 150
+threshold_val = 100
 
 
 def on_trackbar(val):
@@ -84,26 +84,29 @@ if __name__ == '__main__':
     """
    
     capture = cv.VideoCapture(0)
-    window = cv2.namedWindow('window')
-    cv.createTrackbar('threshold', 'window', 150, 255, on_trackbar)
+    window = cv2.namedWindow('digit_recognition')
+    cv.createTrackbar('threshold', 'digit_recognition', 100, 255, on_trackbar)
     while True:
         ret, frame = capture.read()
-        frame = cv.resize(frame, (480, 480))
         window = frame
         # changing the image to grayscale
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         # applying threshold
         _, threshold_frame = cv.threshold(gray_frame, threshold_val, 255, cv.THRESH_BINARY)
+        # cutting a frame in the center
+        cv.rectangle(threshold_frame, (180, 100), (460, 380), (0, 0, 0), thickness=2)
+        cut_frame = threshold_frame[100:380, 180:460]
         # resizing for 28x28
-        resized_frame = cv.resize(threshold_frame, (28, 28), interpolation=cv.INTER_AREA)
+        resized_frame = cv.resize(cut_frame, (28, 28), interpolation=cv.INTER_AREA)
         # test_frame = cv.resize(resized_frame, (480, 480))
         # making it into an array and iverting
         final_frame = np.invert(np.array([resized_frame]))
         
         prediction = loaded_model.predict(final_frame)
         cv.rectangle(threshold_frame, (10, 70), (50, 10), (0, 0, 0), thickness=-1)
-        cv.putText(threshold_frame, str(np.argmax(prediction)), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
-        cv.imshow('window', threshold_frame)
+        cv.putText(threshold_frame, str(np.argmax(prediction)), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2,
+                   (255, 255, 255), thickness=3)
+        cv.imshow('digit_recognition', threshold_frame)
         if cv.waitKey(1) == ord('q'):
             break
     capture.release()
